bc8e0faf74bed499d67a583b256edbe1fc476a52
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-01-04 00:24:21 +0100

    feat: stronger dataloader parametrization

--- src/data/one_urn_datamodule.py
+++ src/data/one_urn_datamodule.py
@@ -10,7 +10,6 @@ from numpy.typing import NDArray
 from torch.utils.data import DataLoader, Dataset
 from typing_extensions import override
 
-from src.utils.imageproc.image_25d import create_25d_image, create_25d_image_with_clahe
 from src.utils.imageproc.resize import open_and_resize
 
 
@@ -54,34 +53,23 @@ class OneUrnDataset(Dataset[Tuple[torch.Tensor, torch.Tensor]]):
     def __init__(
         self,
         image_path: str,
+        slice_jump: int,
+        slice_image_size: int,
         correct_segments_path: str,
         transforms: Compose,
-        rgb_transform: Callable,
         target_transforms: Compose,
-        slice_image_size: Optional[int] = None,
-        slicing_axis: Literal["x", "y", "z"] = "z",
+        projection_axis: Literal["x", "y", "z"] = "z",
     ) -> None:
         self.projection_axis = cast(
-            Literal[0, 1, 2], {k: i for i, k in enumerate(("z", "y", "x"))}[slicing_axis]
+            Literal[0, 1, 2], {k: i for i, k in enumerate(("z", "y", "x"))}[projection_axis]
         )
-        image, urn_slice_size = open_and_resize(
-            Path(image_path), self.projection_axis, slice_image_size
-        )
-        self.image = rgb_transform(image)  # shape (Z, S, S, 3)
-        self.image_slice_number = self.image.shape[self.projection_axis]
-        # WARN: the full resolution for targets has not been kept
-        # what prevents to show the resolution issues of the segmentation
-        correct_segments, gd_slice_size = open_and_resize(
-            Path(correct_segments_path), self.projection_axis, slice_image_size, boolify=True
-        )
-        if urn_slice_size != gd_slice_size:
-            raise Exception(
-                f"""The ground truth and urn tiff files must have the same slice side size. Received {gd_slice_size} and {urn_slice_size}
-
-            See
-            python src/utils.redim_urn.py --help
-            """
-            )
+        self.slice_jump = slice_jump
+        image = open_and_resize(Path(image_path), self.projection_axis, slice_image_size)
+        self.image = image[..., np.newaxis].repeat(3, -1)
+        self.image_slice_number = self.image.shape[self.projection_axis] // self.slice_jump
+        # TODO: read the correct_segments from the image
+        correct_segments_path = correct_segments_path  # unused
+        correct_segments = np.empty_like(image, dtype=np.bool)
         self.correct_segments = correct_segments
         self.transforms = transforms
         self.target_transforms = target_transforms
@@ -91,7 +79,7 @@ class OneUrnDataset(Dataset[Tuple[torch.Tensor, torch.Tensor]]):
         """Return a batch of projections fractioning one urn."""
         # TODO: why taking the first picture instead of the (irpn // pn) ones ?
         # TODO: interpolate
-        idx_to_take = index
+        idx_to_take = index * self.slice_jump
         projection_batch = np.take(self.image, indices=idx_to_take, axis=self.projection_axis)
         correct_segment_batch = np.take(
             self.correct_segments, indices=idx_to_take, axis=self.projection_axis
@@ -131,41 +119,6 @@ def to_tensor(dtype: torch.dtype):
         return torch.as_tensor(x, dtype=dtype)
 
     return to_tensor__forward
-
-
-def to_25dimage(clahe: bool, slice_jump: int):
-    """Convert a 3D volume into a 2D image by extracting the slice at `slice_idx` along the
-    z-axis."""
-
-    def to_25dimage__forward(volume: np.ndarray) -> np.ndarray:
-        new_volume = np.empty((*volume.shape, 3), dtype=volume.dtype)
-        if clahe:
-            for slice_idx in range(volume.shape[0]):
-                new_volume[slice_idx] = create_25d_image_with_clahe(
-                    volume.astype(np.uint8), slice_idx, slice_jump, use_clahe=True
-                )
-        else:
-            for slice_idx in range(volume.shape[0]):
-                new_volume[slice_idx] = create_25d_image(
-                    volume, slice_idx, slice_jump, normalize=False
-                )
-        return new_volume
-
-    return to_25dimage__forward
-
-
-def grayscale_to_rgb():
-    """Duplicate the single channel of the volume to return a grayscale rgb volume."""
-
-    def grayscale_to_rgb__forward(volume: np.ndarray) -> np.ndarray:
-        """Forward function.
-
-        :param volume: shape (Z, S, S)
-        :return: shape (Z, S, S, 3)
-        """
-        return volume[..., np.newaxis].repeat(3, -1)
-
-    return grayscale_to_rgb__forward
 
 
 class OneUrnDataModule(LightningDataModule):
@@ -209,26 +162,21 @@ class OneUrnDataModule(LightningDataModule):
     def __init__(
         self,
         filename: str,
-        ground_truth_filename: str,
         train_val_test_split: Tuple[int, int, int] = (0, 0, 1),
         slice_jump: int = 25,
-        slice_image_size: Optional[int] = None,
+        slice_image_size: int = 640,
         slicing_axis: Literal["x", "y", "z"] = "z",
         projection_batch_size: Optional[int] = None,  # projection frames per batch
-        use_25d_image: Literal["clahe", True, False] = False,
         num_workers: int = 0,
         pin_memory: bool = False,
     ) -> None:
         """Initialize a `OneUrnDataModule`.
 
         :param filename: The file of the urn tiff image.
-        :param ground_truth_filename: The file of the urn ground truth segmentation tiff image.
         :param train_val_test_split: The train, validation and test splits (number of slices). Defaults to `(0, 0, 1)`.
-        :param slice_jump: The number of slices to jump for the 2.5D encoding
-        :param slice_image_size: The width and height value of a projection. If provided, a resized tiff image will be stored on disk. Else, it is expected the urn and ground truth tiff volumes to be isotropic.
-        :param slicing_axis: The axis along which to slice the urn volume. Either `"x"`, `"y"` or `"z"`. Defaults to `"z"`.
+        :param slice_jump: The number of slices to be skipped along the slicing axis
+        :param slice_image_size: The width and height value of a projection. A resized tiff image will be stored on disk.
         :param projection_batch_size: The number of projections per batch. Defaults to `None` to send all projections at once.
-        :param use_25d_image: Whether to use 2.5D images with CLAHE preprocessing (`"clahe"`), without CLAHE (`True`).
         :param num_workers: The number of workers. Defaults to `0`.
         :param pin_memory: Whether to pin memory. Defaults to `False`.
         """
@@ -246,15 +194,6 @@ class OneUrnDataModule(LightningDataModule):
                 normalize_channel(),
                 to_tensor(torch.float32),
             ]
-        )
-        self.rgb_transform = (
-            to_25dimage(clahe=True, slice_jump=slice_jump)
-            if use_25d_image == "clahe"
-            else (
-                to_25dimage(clahe=False, slice_jump=slice_jump)
-                if use_25d_image
-                else grayscale_to_rgb()
-            )
         )
         self.target_transforms = Compose([to_tensor(torch.bool)])
 
@@ -277,13 +216,13 @@ class OneUrnDataModule(LightningDataModule):
         Do not use it to assign state (self.x = y).
         """
         self._cached_data_test = OneUrnDataset(
-            image_path=cast(str, self.hparams.get("filename")),
-            slice_image_size=cast(Optional[int], self.hparams.get("slice_image_size")),
-            correct_segments_path=cast(str, self.hparams.get("ground_truth_filename")),
-            transforms=self.transforms,
-            rgb_transform=self.rgb_transform,
-            target_transforms=self.target_transforms,
-            slicing_axis=cast(Literal["x", "y", "z"], self.hparams.get("slicing_axis")),
+            cast(str, self.hparams.get("filename")),
+            cast(int, self.hparams.get("slice_jump")),
+            cast(int, self.hparams.get("slice_image_size")),
+            "incorrect_path",  # TODO: set a real file path
+            self.transforms,
+            self.target_transforms,
+            cast(Literal["x", "y", "z"], self.hparams.get("slicing_axis")),
         )
 
     def setup(self, stage: Optional[str] = None) -> None:
--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -1,35 +1,13 @@
 from pathlib import Path
-from typing import Literal
 
-import numpy as np
 import pytest
-import tifffile
 import torch
 
 from src.data.one_urn_datamodule import OneUrnDataModule
 
 
-def generate_mock_urn_data(
-    data_dir: str, filestem: str, gd_filestem: str, urn_shape: tuple[int, int, int]
-) -> tuple[Path, Path]:
-    """Write a mock urn in a file and a mock ground truth in another."""
-    urn = np.random.randint(0, 256, urn_shape)
-    urn_gd = (np.random.rand(*urn_shape) > 0.5) * 255
-    urn_path = Path(data_dir) / f"{filestem}.tiff"
-    urn_gd_path = Path(data_dir) / f"{gd_filestem}.tiff"
-    tifffile.imwrite(urn_path, urn)
-    tifffile.imwrite(urn_gd_path, urn_gd)
-    return urn_path, urn_gd_path
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    ["slice_jump", "batch_size", "use_25d_image"],
-    [(5, 1, False), (10, 1, False), (5, 5, False), (10, 5, False), (5, 5, True), (5, 5, "clahe")],
-)
-def test_one_urn_datamodule(
-    slice_jump: int, batch_size: int, use_25d_image: Literal["clahe", True, False]
-) -> None:
+@pytest.mark.parametrize(["slice_jump", "batch_size"], [(5, 1), (10, 1), (5, 5), (10, 5)])
+def test_one_urn_datamodule(slice_jump: int, batch_size: int) -> None:
     """Tests `OneUrnDataModule` to verify that a test tiff file can be opened correctly, that the
     necessary attributes were created (e.g., the dataloader objects), and that dtypes and batch
     sizes correctly match.
@@ -40,37 +18,24 @@ def test_one_urn_datamodule(
     data_dir = "data/"
 
     mock_urn_filestem = "test_urn"
-    mock_urn_gd_filestem = "test_urn_gd"
-    slice_image_size = 32
+    mock_urn = Path(data_dir) / f"{mock_urn_filestem}.tiff"
     mock_urn_dim = 32
-    mock_urn_shape = (mock_urn_dim, slice_image_size, slice_image_size)
-    mock_urn, mock_urn_gd = generate_mock_urn_data(
-        data_dir, mock_urn_filestem, mock_urn_gd_filestem, mock_urn_shape
-    )
+    slice_image_size = 32
     slicing_axis, slicing_axis_idx = "z", 0
     dm = OneUrnDataModule(
         filename=str(mock_urn),
-        ground_truth_filename=str(mock_urn_gd),
         train_val_test_split=(0, 0, 1),
         slice_jump=slice_jump,
         slice_image_size=slice_image_size,
         projection_batch_size=batch_size,
         slicing_axis=slicing_axis,
-        use_25d_image=use_25d_image,
     )
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_val and not dm.data_test
-    resized_urn_file = Path(
+    assert Path(
         data_dir, f"{mock_urn_filestem}__{slice_image_size}_{slicing_axis_idx}.tiff"
-    )
-    resized_gd_file = Path(
-        data_dir, f"{mock_urn_gd_filestem}__{slice_image_size}_{slicing_axis_idx}.tiff"
-    )
-    assert resized_urn_file.exists()
-    resized_urn_file.unlink(True)
-    assert resized_gd_file.exists()
-    resized_gd_file.unlink(True)
+    ).exists()
 
     dm.setup()
     # zero-shot for now
@@ -86,11 +51,6 @@ def test_one_urn_datamodule(
     batch = next(iter(dm.test_dataloader()))
     x, y = batch
     assert len(x) == min(batch_size, total_slice_nb)
-    assert x.shape[1:] == (3, slice_image_size, slice_image_size)
     assert len(y) == min(batch_size, total_slice_nb)
-    assert y.shape[1:] == (slice_image_size, slice_image_size)
     assert x.dtype == torch.float32
     assert y.dtype == torch.bool
-
-    mock_urn.unlink(True)
-    mock_urn_gd.unlink(True)
