da44730e4fcf0f743a494160e9100dc8f44da444
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-01-04 00:52:36 +0100

    feat: ground truth image loading
    NB: the image is resized as with the image to be segmented

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -18,12 +18,15 @@ def test_one_urn_datamodule(slice_jump: int, batch_size: int) -> None:
     data_dir = "data/"
 
     mock_urn_filestem = "test_urn"
+    mock_urn_gd_filestem = "test_urn_gd"
     mock_urn = Path(data_dir) / f"{mock_urn_filestem}.tiff"
+    mock_urn_gd = Path(data_dir) / f"{mock_urn_gd_filestem}.tiff"
     mock_urn_dim = 32
     slice_image_size = 32
     slicing_axis, slicing_axis_idx = "z", 0
     dm = OneUrnDataModule(
         filename=str(mock_urn),
+        ground_truth_filename=str(mock_urn_gd),
         train_val_test_split=(0, 0, 1),
         slice_jump=slice_jump,
         slice_image_size=slice_image_size,
@@ -35,6 +38,9 @@ def test_one_urn_datamodule(slice_jump: int, batch_size: int) -> None:
     assert not dm.data_train and not dm.data_val and not dm.data_test
     assert Path(
         data_dir, f"{mock_urn_filestem}__{slice_image_size}_{slicing_axis_idx}.tiff"
+    ).exists()
+    assert Path(
+        data_dir, f"{mock_urn_gd_filestem}__{slice_image_size}_{slicing_axis_idx}.tiff"
     ).exists()
 
     dm.setup()
@@ -51,6 +57,8 @@ def test_one_urn_datamodule(slice_jump: int, batch_size: int) -> None:
     batch = next(iter(dm.test_dataloader()))
     x, y = batch
     assert len(x) == min(batch_size, total_slice_nb)
+    assert x.shape[1:] == (3, slice_image_size, slice_image_size)
     assert len(y) == min(batch_size, total_slice_nb)
+    assert y.shape[1:] == (slice_image_size, slice_image_size)
     assert x.dtype == torch.float32
     assert y.dtype == torch.bool
